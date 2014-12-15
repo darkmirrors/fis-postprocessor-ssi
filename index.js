cfcbca79f25e091fdeaebc1075b460869525553b
darkmirrors <darkmirrors@163.com>
2014-12-15 18:06:12 +0800

    更新缓存机制，修改include文件也可以正常嵌入到引入文件中
    Thanks hefangshi

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+'use strict';
+
+function addDeps(a, b){
+    if(a && a.cache && b){
+        if(b.cache){
+            a.cache.mergeDeps(b.cache);
+        }
+        a.cache.addDeps(b.realpath || b);
+    }
+}
+
 module.exports = function(content, file, conf) {
     var fileReg = conf.reg || /<!--#include\svirtual="([^"]+)"\s*-->/gim;
 
@@ -9,6 +20,9 @@ module.exports = function(content, file, conf) {
 
             if (fis.util.isFile(src)) {
                 var tpl = fis.file.wrap(src);
+
+                addDeps(file, tpl);
+
                 return tpl.getContent();
             }
         }
